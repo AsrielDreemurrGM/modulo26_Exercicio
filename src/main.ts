4a640d86d5e8fe38aa97b584c6c3740394e27029
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2024-11-09 01:14:11 -0300

    Criado Funções "multiplicacao" e "dizOla"
    - Instalado TypeScript;
    - Configurado Diretório de Saída Para o TypeScript;
    - Criado .gitignore;

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+function multiplicacao(primNumero: number, segNumero: number): number {
+    return primNumero * segNumero;
+}
+
+console.log(multiplicacao(10, 10));
+
+function dizOla(nome: string): string {
+    let saudacao: string = "Olá ";
+    let saudacaoCompleta: string = saudacao + nome;
+
+    // Também é possível fazer sem a variável "saudacao";
+    // let saudacaoCompleta: string = "Olá " + nome;
+
+    return saudacaoCompleta;
+}
+
+console.log(dizOla("Maria"));
